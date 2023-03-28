b3908a1dd870a32df74c87382b679668c70f020e
danisr1392 <dannyserrano1392@gmail.com>
2023-03-28 16:43:05 +0200

    commit cualquiera

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
+$(function(){
+
     $(".text-slider").hide();
-})
+
+    $(".text-slider:first").show();
+
+    /*botones slider*/
+    $(".select-slider li").click(function(){
+        
+        $(".select-slider li").removeClass();
+        $(this).addClass("active-slider");
+
+        var id = $(".select-slider .active-slider").attr("id");
+
+        $(".text-slider").hide();
+
+        $(".text-slider:nth-child(" + id + ")").fadeIn("slow");
+    });
+
+    
+});
